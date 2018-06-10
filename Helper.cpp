d3fef72915bcaae296415595144be967205c6efd
hareld10 <harel91827364>
2018-06-10 19:33:41 +0300

    with files

---
+++ Helper.cpp
@@ -0,0 +1,19 @@
+//
+// Created by hareld10 on 6/10/18.
+//
+
+#include "Helper.h"
+
+template<typename Out>
+void Helper::split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+std::vector<std::string> Helper::split(std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
---
+++ Helper.h
@@ -0,0 +1,19 @@
+//
+// Created by hareld10 on 6/10/18.
+//
+
+#ifndef EX4_HELPER_H
+#define EX4_HELPER_H
+#include <string>
+#include <vector>
+template<typename Out>
+class Helper {
+public:
+    void Helper::split(const std::string &s, char delim, Out result) ;
+    std::vector<std::string> Helper::split(std::string &s, char delim);
+};
+
+
+
+
+#endif //EX4_HELPER_H
